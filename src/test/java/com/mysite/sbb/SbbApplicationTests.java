9d6d679057dca7a8e1a3ef1aa521806c119380e2
dfajskl4865 <dfajskl4865@gmail.com>
2022-04-23 15:35:46 +0900

    2-05 리포지터리, 데이터 저장하기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,10 +1,31 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testJps(){
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("sbb가 무엇인가요?");
+		q2.setContent("sbb에 대해서 알고 싶습니다.");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
+	}
 
 	@Test
 	void contextLoads() {
