88472fe6f1f6ace9827d7c21386f63fb33582e16
dfajskl4865 <dfajskl4865@gmail.com>
2022-04-24 20:57:02 +0900

    2-15 질문 등록과 폼, 질문등록

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,5 +31,16 @@ public class QuestionController {
         model.addAttribute("question",question);
 
         return "question_detail";
+    }
+
+    @RequestMapping("/create")
+    public String questionCreate(){
+        return "question_from";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        questionService.create(subject,content);
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
